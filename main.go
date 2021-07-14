a0dba4efd00b62047a864706373935d0e8e4f8eb
yoonhero <yoonhero06@naver.com>
2021-07-14 23:52:30 +0900

    #4.5

--- main.go
+++ main.go
@@ -8,5 +8,12 @@ import (
 
 func main() {
 	chain := blockchain.GetBlockchain()
-	fmt.Println(chain)
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AllBlocks()
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
 }
