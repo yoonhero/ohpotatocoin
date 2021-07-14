d85a56677a20ca6bb3c1f102c0fde4df0a8d6cb4
yoonhero <yoonhero06@naver.com>
2021-07-14 15:31:37 +0900

    #0.08

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) sayHello() {
+	fmt.Printf("Hello my name is %s and I'm %d", p.name, p.age-1)
+}
+
 func main() {
-	a := 2
-	b := &a
-	fmt.Println(*b, &a)
-	fmt.Scanf("%d", &a)
-	fmt.Println(a)
+	yoonhero := person{name: "ysh", age: 12}
+	yoonhero.sayHello()
 }
