1a338268bbd9b9afbad26fb82b2639050411e3be
yoonhero <yoonhero06@naver.com>
2021-07-14 14:57:53 +0900

    #0.01

--- main.go
+++ main.go
@@ -6,8 +6,16 @@ func plus(a, b int, name string) (int, string) {
 	return a + b, name
 }
 
+func plusAll(a ...int) int {
+	var total int
+	for _, item := range a {
+		total += item
+	}
+	return total
+}
+
 func main() {
 	//var name string = "hello"
-	result, name := plus(2, 2, "nico")
-	fmt.Println(result, name)
+	result := plusAll(2, 3, 4, 5, 6, 7, 8, 9)
+	fmt.Println(result)
 }
