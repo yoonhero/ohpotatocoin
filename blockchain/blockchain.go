bac3b4ae95f58bf0ebfeaa7d7f0859a1e6843674
yoonhero <yoonhero06@naver.com>
2021-07-14 23:33:12 +0900

    #4.3

---
+++ blockchain/blockchain.go
@@ -0,0 +1,21 @@
+package blockchain
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+type blockchain struct {
+	blocks []block
+}
+
+var b *blockchain
+
+func GetBlockchain() *blockchain {
+	if b == nil {
+		b = &blockchain{}
+	}
+	return b
+
+}
