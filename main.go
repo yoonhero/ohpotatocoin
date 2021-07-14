e545ab66e4f6386c2dfd5c53cf77d523bcbcc609
yoonhero <yoonhero06@naver.com>
2021-07-14 15:18:00 +0900

    #0.06

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	foods := [3]string{"potato", "pizza", "pasta"}
-	for i := 0; i < len(foods); i++ {
-		fmt.Println(foods[i])
-	}
+	foods := []string{"potato", "pizza", "pasta"}
+	fmt.Printf("%v\n", foods)
+	foods = append(foods, "tomato")
+	fmt.Printf("%v\n", foods)
 }
