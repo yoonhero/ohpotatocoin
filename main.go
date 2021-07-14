c7a8c04370ddf4f14635a28f95efd3ab3d9add32
yoonhero <yoonhero06@naver.com>
2021-07-14 15:04:18 +0900

    #0.02

--- main.go
+++ main.go
@@ -2,20 +2,9 @@ package main
 
 import "fmt"
 
-func plus(a, b int, name string) (int, string) {
-	return a + b, name
-}
-
-func plusAll(a ...int) int {
-	var total int
-	for _, item := range a {
-		total += item
-	}
-	return total
-}
-
 func main() {
-	//var name string = "hello"
-	result := plusAll(2, 3, 4, 5, 6, 7, 8, 9)
-	fmt.Println(result)
+	name := "HI my name is ysh"
+	for _, letter := range name {
+		fmt.Printf("%x\n", letter)
+	}
 }
