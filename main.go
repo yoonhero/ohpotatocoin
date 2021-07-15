73f6939d6afbd48626af96be31df24aff9c54831
yoonhero <yoonhero06@naver.com>
2021-07-15 22:57:47 +0900

    #7.1

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"github.com/yoonhero/ohpotatocoin/explorer"
-	"github.com/yoonhero/ohpotatocoin/rest"
+	"fmt"
+	"os"
 )
 
+// introduce usages
+func usage() {
+	fmt.Printf("Welcome to 오감자 코인 \n")
+	fmt.Printf("Please use the following commands: \n\n")
+	fmt.Printf("explorer:    Start the HTML Explorer\n")
+	fmt.Printf("rest:        Start the Rest API (recommended)\n")
+	// exit program not to occur err
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(5000)
-	rest.Start(4000)
+	// if don't type like rest or explorer
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	// if os.Args[1] == 'explorer'
+	case "explorer":
+		fmt.Println("Start Explorer")
+
+	// if os.Args[1] == "rest"
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }
