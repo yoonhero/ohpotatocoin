2011e80e0a5b440cc62e3c4f137e3e0179f828c6
yoonhero <yoonhero06@naver.com>
2021-07-15 13:30:07 +0900

    #6.0

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "github.com/yoonhero/ohpotatocoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/yoonhero/ohpotatocoin/utils"
+)
+
+const port string = ":5000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s\n", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
