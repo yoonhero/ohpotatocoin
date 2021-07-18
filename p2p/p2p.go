e321104ea9445ff1deef6554bdba8fc7baa575b6
yoonhero <yoonhero06@naver.com>
2021-07-19 08:11:38 +0900

    #12.6

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -10,7 +11,16 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	_, err := upgrader.Upgrade(rw, r, nil)
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-
+	for {
+		fmt.Println("Waiting for message ...")
+		_, p, err := conn.ReadMessage()
+		fmt.Println("Message arrived")
+		utils.HandleErr(err)
+		fmt.Printf("%s\n\n", p)
+	}
 }
