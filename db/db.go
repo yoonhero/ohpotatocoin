0240b550fc63e1adfdec6302db7b7df0f45f590c
yoonhero <yoonhero06@naver.com>
2021-08-13 16:54:08 +0900

    #13.12

--- db/db.go
+++ db/db.go
@@ -26,15 +26,19 @@ type DB struct {
 func (DB) FindBlock(hash string) []byte {
 	return findBlock(hash)
 }
+
 func (DB) LoadChain() []byte {
 	return loadChain()
 }
+
 func (DB) SaveBlock(hash string, data []byte) {
 	saveBlock(hash, data)
 }
+
 func (DB) SaveChain(data []byte) {
 	saveChain(data)
 }
+
 func (DB) DeleteAllBlocks() {
 	emptyBlocks()
 }
